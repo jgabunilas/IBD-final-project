654c9a0af39552082e3e6458215f6430e1f6dc7b
Autonomousse <ucsddatacamp@gmail.com>
2020-11-15 15:42:30 -0800

    RFECV feature test notebook with results

--- tpot_best_model_5_features.py
+++ tpot_best_model_5_features.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.svm import LinearSVC
-from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,13 +9,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.9187730513263921
-exported_pipeline = make_pipeline(
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    LinearSVC(C=25.0, dual=False, loss="squared_hinge", penalty="l1", tol=0.0001)
-)
-# Fix random state for all the steps in exported pipeline
-set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+# Average CV score on the training set was: 0.9171331220728208
+exported_pipeline = LogisticRegression(C=0.0001, dual=False, penalty="l2")
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
